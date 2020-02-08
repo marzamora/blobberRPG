222050e51698e6c7c4df20a65ab77c7efda334f0
Marcello Zamora <zamoramaj@gmail.com>
2020-02-07 20:54:03 -0500

    Added Game script and edited GUI scene.

---
+++ Game.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# Script for handling transition between Menus, Maps, and Combat.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
